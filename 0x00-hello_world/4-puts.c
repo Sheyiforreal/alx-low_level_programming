c09d22c910c16460fae4306c2d3c6380bfef1115
Oluwaseyi Owolabi <sheyirealowolabi@gmail.com>
2022-09-08 09:50:50 -0700

    my puts commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-/**
- *
+/*
  *  * main - Entry point
  *
- *   *
+ *  
  *
  *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
@@ -16,5 +14,4 @@ int main(void)
 		puts("\"Programming is like building a multilingual puzzle");
 
 			return (0);
-
 }
