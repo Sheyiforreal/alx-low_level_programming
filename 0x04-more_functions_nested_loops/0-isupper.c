0eb0f5df0dc80adf2b09bdef25b04e5a6cc1cc7a
Sheyiforreal <sheyirealowolabi@gmail.com>
2022-09-16 04:06:23 +0100

    isupper commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
  * _isupper - function that checks for uppercase character.
  * @c: int type number
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
 	if (c >= 65 && c <= 91)
